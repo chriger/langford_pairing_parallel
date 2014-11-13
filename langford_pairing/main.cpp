55b129da0b57bf59e7579c6357f1b5a032ff706c
cgerloff <cgerloff@fuchs.de>
2014-11-13 14:33:54 +0100

    Initial Commit

---
+++ langford_pairing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  langford_pairing
+//
+//  Created by Gerloff, Christian on 13.11.14.
+//  Copyright (c) 2014 Gerloff, Christian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
