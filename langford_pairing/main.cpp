2ce8594ea7262ede19124192d1508c3a244963d6
cgerloff <cgerloff@fuchs.de>
2014-11-19 13:00:18 +0100

    Initial commit

--- langford_pairing/main.cpp
+++ langford_pairing/main.cpp
@@ -7,9 +7,26 @@
 //
 
 #include <iostream>
+#include "tbb/tbb.h"
+#include "langford_solver.h"
+using namespace tbb;
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // Define how many threads should be used
+    tbb::task_scheduler_init init(4);
+    
+    // Size of the langford pairing problem 4m & 4m-1 [3, 4, 7, 8, 11, 12, 15, 16, 19, 20, 23, 24,...]
+    char n = 8;
+    
+    tick_count t0 = tick_count::now();
+    LangfordSolver* root = new(tbb::task::allocate_root()) LangfordSolver(0, 1, 2*n);
+    tbb::task::spawn_root_and_wait(*root);
+    
+    tick_count t1 = tick_count::now();
+    
+    printf("Zeit für Algorithmus: %g seconds\n", (t1-t0).seconds());
+    
+    cout << "Lösungen: " << LangfordSolver::getSolutionCounter() << endl;
     return 0;
 }
